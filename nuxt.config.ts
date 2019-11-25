119787e19a38d8d108e1df0b8824ce7efea1f0ea
HikaruEgashira <shighten@gmail.com>
2019-11-25 22:42:16 +0900

    meta情報の修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,48 @@
 import NuxtConfiguration from "@nuxt/config";
-const pkg = require("./package");
+const baseName = process.env.BASE_NAME || "Twin:te";
+const baseDesc =
+  process.env.BASE_DISC || "筑波大学専門の時間割アプリケーションです";
+const baseUrl = process.env.BASE_URL || "http://localhost:3000";
+const baseDir = process.env.BASE_DIR || "/";
 
 const config: NuxtConfiguration = {
   mode: "spa",
   head: {
-    title: pkg.name,
     htmlAttrs: {
-      lang: "ja"
+      lang: "ja",
+      prefix: "og: http://ogp.me/ns#"
     },
+    title: baseName,
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "Twin:teは筑波大学生向けの時間割作成アプリです。現在Playストア及びAppStoreでは配布していませんのでこちらからお試しください！" }
+      { hid: "description", name: "description", content: baseDesc },
+      { hid: "og:site_name", property: "og:site_name", content: baseName },
+      { hid: "og:type", property: "og:type", content: "article" },
+      { hid: "og:url", property: "og:url", content: baseUrl },
+      { hid: "og:title", property: "og:title", content: baseName },
+      { hid: "og:description", property: "og:description", content: baseDesc },
+      {
+        hid: "og:image",
+        property: "og:image",
+        content: `${baseUrl}/icon.png`
+      },
+      { name: "twitter:card", content: baseDesc },
+      { name: "twitter:site", content: "@te_twin" }
     ]
+  },
+  router: {
+    base: baseDir
+  },
+  loadingIndicator: {
+    name: "wandering-cubes",
+    color: "teal",
+    color2: "white",
+    background: "gray"
   },
   loading: {
     color: "teal",
-    height: "5px",
-    continuous: true
+    height: "5px"
   },
   modules: ["nuxt-buefy"],
   build: {
@@ -30,9 +55,30 @@ const config: NuxtConfiguration = {
     },
     optimizeCSS: true
   },
+  workbox: {
+    dev: false,
+    runtimeCaching: [
+      {
+        urlPattern: "^https://fonts.(?:googleapis|gstatic).com/(.*)",
+        handler: "cacheFirst"
+      },
+      {
+        urlPattern: baseDir + "lib/.*",
+        handler: "cacheFirst",
+        strategyOptions: {
+          cacheName: "image-cache",
+          cacheExpiration: {
+            maxEntries: 20,
+            maxAgeSeconds: 24 * 60 * 60 * 30
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     { src: "~~/node_modules/vue-rellax/lib/nuxt-plugin", ssr: false },
-    "~~/plugins/laxxx"
+    "~~/plugins/laxxx",
+    { src: "~~/plugins/ga.js", mode: "client" }
   ],
   css: ["~assets/css/main.scss"]
 };
