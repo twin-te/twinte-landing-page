c91fe0110223b8c6af3b7d559b4ce643491f0c0b
HikaruEgashira <shighten@gmail.com>
2021-03-22 12:26:06 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Twin:te Landing Page V3
---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>Hello😎</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {},
+});
+</script>
+
+<style></style>
