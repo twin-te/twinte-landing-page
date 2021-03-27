42a61688af21d5c77d9a8bcf82003da67b19dae2
HikaruEgashira <shighten@gmail.com>
2021-03-27 14:17:13 +0900

    Add Section Link

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
-<template>Hello😎</template>
+<template>
+  <router-view />
+</template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "App",
-  components: {},
 });
 </script>
 
---
+++ src/components/SectionTitle.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1 class="section-title">{{ title }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SectionTitle",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import "~/scss/main.scss";
+
+.section-title {
+  @include center-flex(column);
+  @include heading-1;
+  margin-bottom: 6rem;
+
+  &::after {
+    content: "";
+    margin-top: 1rem;
+    width: 2.4rem;
+    height: 0.5rem;
+    background: $primary-liner;
+  }
+}
+</style>
