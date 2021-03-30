9f541eac963e11924b97990153ca79f75dca3c6a
Hayato Shishido <hayabusax555@yahoo.co.jp>
2021-03-31 00:43:57 +0900

    add header-memu

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,19 +1,51 @@
 <template>
+  <HeaderMenu v-model:inTop="inTop" />
+  <!-- 後で消す -->
+  <div @click="clickHandler" class="image"></div>
+  <div class="image-bottom"></div>
   <Link />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onMounted, ref, watchEffect } from "vue";
 import Link from "~/organisms/Link.vue";
+import HeaderMenu from "~/organisms/HeaderMenu.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
+    HeaderMenu,
     Link,
+  },
+  setup: () => {
+    const inTop = ref(false);
+
+    onMounted(() => {
+      const observer = new IntersectionObserver((entries) => {
+        watchEffect(() => {
+          inTop.value = entries[0].isIntersecting;
+        });
+      });
+      const target = document.querySelector(".image");
+      observer.observe(target as Element);
+    });
+
+    return { inTop };
   },
 });
 </script>
 
 <style lang="scss" scoped>
 @import "~/scss/main.scss";
+
+// 後で消す
+.image {
+  width: 100%;
+  height: 10rem;
+  background: $primary-liner;
+}
+.image-bottom {
+  height: 30rem;
+  background: $base;
+}
 </style>
