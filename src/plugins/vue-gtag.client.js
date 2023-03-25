7a7eaf265e0003f3f97b67f012d738393ee82b0c
appare <64473501+appare45@users.noreply.github.com>
2023-03-25 11:38:14 +0900

    refactor: #89 製品版ビルドでのみGoogle Tag Managerを有効に

--- src/plugins/vue-gtag.client.js
+++ src/plugins/vue-gtag.client.js
@@ -3,8 +3,9 @@ import VueGtag, { trackRouter } from 'vue-gtag-next'
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGtag, {
     property: {
-      id: 'GTM-W65BSBG'
-    }
+      id: 'GTM-W65BSBG',
+    },
+    isEnabled: import.meta.env.PROD, 
   })
   trackRouter(useRouter())
 })
