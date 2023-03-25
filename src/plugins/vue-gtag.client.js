91912882d05b653da26574f16e8b78231c6437bd
appare <64473501+appare45@users.noreply.github.com>
2023-03-25 11:13:35 +0900

    feat: #89 Google tag manager を導入

---
+++ src/plugins/vue-gtag.client.js
@@ -0,0 +1,10 @@
+import VueGtag, { trackRouter } from 'vue-gtag-next'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(VueGtag, {
+    property: {
+      id: 'GTM-W65BSBG'
+    }
+  })
+  trackRouter(useRouter())
+})
